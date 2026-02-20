a943e81be9ac610ebce86e7f340373e466eca476
admzulox <admzulox@gmail.com>
2026-02-19 23:51:33 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 ADICIONE ISSO AQUI
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
   },
   server: {
     port: 3000,
+    host: '0.0.0.0',
+  },
+  define: {
+    'process.env': {}
   }
 });
